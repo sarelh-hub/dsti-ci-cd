541030d9088937493aa4a0ae984727c3badd5d53
EL HAIMER <sara.el-haimer@edu.dsti.institute>
2025-05-03 22:54:45 +0200

    First Commit

---
+++ src/user_functions.py
@@ -0,0 +1,39 @@
+import re
+
+def is_valid_password(password):
+    if len(password) < 8:
+        return False
+    if not re.search(r"[A-Za-z]", password):  # au moins une lettre
+        return False
+    if not re.search(r"\d", password):        # au moins un chiffre
+        return False
+    if not re.search(r"[^\w\s]", password):   # au moins un caractère spécial (non lettre, non chiffre, non espace)
+        return False
+    return True
+
+
+
+def get_email_from_input():
+    """ Contains '@' and '.' """
+    email = input("Tell me your email: ")
+
+    if ("@" not in email or "." not in email):
+        print('Email is not valid.')
+    else:
+        return email
+    
+def get_user_name_from_input():
+    """ Not empty string. No spaces. """
+    user_name =  input("Create your user name: ")
+    if (len(user_name) == 0 or " " in user_name):
+        print("User Name is not valid.")
+    else:
+        return user_name
+
+def get_password_from_input():
+    """ Password needs to be at least 8 characters long with at least one number, one special character and one letter. """
+    password =  input("Create your password: ")
+    if not is_valid_password(password):
+        print("Password needs to be at least 8 characters long with at least one number, one special character and one letter.")
+    else:
+        return password
---
+++ test/test_user_functions.py
@@ -0,0 +1,46 @@
+import sys
+# Always run from unit_testing_best_practice/test
+sys.path += ['../src']
+
+import pytest
+import io
+from user_functions import *
+
+
+def test_email_with_user_input_no_at_sign(monkeypatch):
+    monkeypatch.setattr('sys.stdin', io.StringIO('bruce,wayne2wayneenterprises,com'))
+    assert get_email_from_input() is None
+
+def test_email_with_user_input_no_dot(monkeypatch):
+    monkeypatch.setattr('sys.stdin', io.StringIO('brucewayne@wayneenterprises,com'))
+    assert get_email_from_input() is None
+
+def test_email_with_user_input_correct(monkeypatch):
+    monkeypatch.setattr('sys.stdin', io.StringIO('bruce.wayne@wayneenterprisescom'))
+    assert get_email_from_input() == 'bruce.wayne@wayneenterprisescom'
+
+# Do the same for the following functions
+# Functions in src/user_functions.py and tests in tests/test_user_functions.py
+
+def test_user_name_input_with_spaces(monkeypatch):
+    monkeypatch.setattr('sys.stdin', io.StringIO('Sa ra'))
+    assert get_user_name_from_input() is None
+
+def test_user_name_input_empty_string(monkeypatch):
+    monkeypatch.setattr('sys.stdin', io.StringIO('\n'))
+    assert get_user_name_from_input() is None
+
+def test_user_name_input_correct(monkeypatch):
+    monkeypatch.setattr('sys.stdin', io.StringIO('Sara'))
+    assert get_user_name_from_input() == "Sara"
+
+def test_password_input_less_eight_ch_no_number_no_special_ch(monkeypatch):
+    """ Password needs to be at least 8 characters long with at least one number, one special character and one letter. """
+    monkeypatch.setattr('sys.stdin', io.StringIO('azerty'))
+    assert get_password_from_input() is None
+
+def test_password_input_correct(monkeypatch):
+    """ Password needs to be at least 8 characters long with at least one number, one special character and one letter. """
+    monkeypatch.setattr('sys.stdin', io.StringIO('azerty1+'))
+    assert get_password_from_input() == "azerty1+"
+
